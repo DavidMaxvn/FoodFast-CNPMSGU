890eabd4f6f9b5db740fd6e97355eded995a97fb
DavidMaxvn <162657738+DavidMaxvn@users.noreply.github.com>
2025-10-21 21:41:10 +0700

    New changes!

--- src/main/java/com/fastfood/management/dto/response/VNPayResponse.java
+++
@@ -1,9 +0,0 @@
-package com.fastfood.management.dto.response;
-
-import lombok.Data;
-
-@Data
-public class VNPayResponse {
-    private String paymentUrl;
-    private String transactionReference;
-}
